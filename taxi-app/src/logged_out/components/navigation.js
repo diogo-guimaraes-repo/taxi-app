d81036ccc000c25c1f6d43971476e25d5e20409a
Diogo <diogo.m.guimaraes.92@gmail.com>
2021-08-23 16:09:26 +0100

    Home page start

---
+++ taxi-app/src/logged_out/components/navigation.js
@@ -0,0 +1,9 @@
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+      <div className='container'>
+
+      </div>
+    </nav>
+  )
+}
