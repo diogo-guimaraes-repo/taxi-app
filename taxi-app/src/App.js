ce088a49ce7e3886c1cd7946007248aec6918b10
Diogo <diogo.m.guimaraes.92@gmail.com>
2021-09-03 14:56:19 +0100

    add auto scroll

--- taxi-app/src/App.js
+++ taxi-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GlobalStyle from './globalStyles';
 import Home from './pages/HomePage/Home';
+import ScrollToTop from './components/ScrollToTop';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {Footer, Navbar} from './components';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <GlobalStyle/>
+      <ScrollToTop/>
       <Navbar/>
       <Switch>
         <Route path='/' exact component={Home}/>
